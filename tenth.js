17d962ccfaeb5eaeab5019ccfddfab6af26529d8
aliazam <aliazam@hotmail.co.uk>
2016-06-18 02:05:37 +0500

    added some file in tenth html

---
+++ tenth.js
@@ -0,0 +1,13 @@
+var a=prompt("what is your age?");
+if (a<"12"){
+    alert('less then 12');
+}
+else if (a>"12" && a<="18") {
+    alert("less than or equal to 18");    
+}
+else if (a < "0") {
+    document.write("you must be kidding right");
+}
+else {
+    alert('great do what ever you want to :D ');
+}
